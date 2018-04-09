4fb54f45903227a13d171eeb6ddc1c937d4eb4b9
MrNasch <nicolaspaschal@hotmail.com>
2018-04-09 11:41:29 +0200

    added caracters function

--- magicFight/magicFight/ViewController.swift
+++ magicFight/magicFight/ViewController.swift
@@ -14,27 +14,35 @@ class ViewController: UIViewController {
     var newGame : String = "Welcome to MagicFight please select 3 caracters"
     
     // creat the fighter
-    var fighter = Fighter()
-    fighter.name = "pierre"
-    fighter.life = 100
-    fighter.attack = 10
+    func creatFighter() {
+        var fighter = Fighter()
+        fighter.name = "pierre"
+        fighter.life = 100
+        fighter.attack = 10
+    }
     
     // creat the magus
-    var magus = Magus()
-    magus.name = "paul"
-    magus.life = 120
-    magus.heal = 8
+    func creatMagus() {
+        var magus = Magus()
+        magus.name = "paul"
+        magus.life = 120
+        magus.heal = 8
+    }
     
     // creat the colossus
-    var colossus = Colossus()
-    colossus.name = "jacques"
-    colossus.life = 180
-    colossus.attack = 5
+    func creatColossus() {
+        var colossus = Colossus()
+        colossus.name = "jacques"
+        colossus.life = 180
+        colossus.attack = 5
+    }
     
     // creat the Dwarf
-    var dwarf = Dwarf()
-    dwarf.name = "michel"
-    dwarf.life = 70
-    dwarf.attack = 25
+    func creatDwarf() {
+        var dwarf = Dwarf()
+        dwarf.name = "michel"
+        dwarf.life = 70
+        dwarf.attack = 25
+    }
 }
 
