1828cd81bd3fdedcf030b4541689ca247d684de2
MrNasch <nicolaspaschal@hotmail.com>
2018-04-09 15:37:57 +0200

    added caracter class

--- magicFight/magicFight/ViewController.swift
+++ magicFight/magicFight/ViewController.swift
@@ -13,45 +13,53 @@ class ViewController: UIViewController {
     // created var newGame imported when you creat a new game
     var newGame : String = "Welcome to MagicFight please select 3 caracters"
     
-    // creat function fighter
-    func creatFighter() {
-        var fighter = Fighter()
-        fighter.name = "pierre"
-        fighter.life = 100
-        fighter.attack = 10
-        
-        creatFighter()
+    //creat class fighter
+    class fighter {
+        // creat function fighter
+        func creatFighter() {
+            var fighter = Fighter()
+            fighter.name = "pierre"
+            fighter.life = 100
+            fighter.attack = 10
+            
+            creatFighter()
+        }
     }
-    
-    // creat function magus
-    func creatMagus() {
-        var magus = Magus()
-        magus.name = "paul"
-        magus.life = 120
-        magus.heal = 15
-        
-        creatMagus()
+    //creat class magus
+    class magus {
+        // creat function magus
+        func creatMagus() {
+            var magus = Magus()
+            magus.name = "paul"
+            magus.life = 120
+            magus.heal = 15
+            
+            creatMagus()
+        }
     }
-    
-    // creat function colossus
-    func creatColossus() {
-        var colossus = Colossus()
-        colossus.name = "jacques"
-        colossus.life = 180
-        colossus.attack = 5
-        
-        creatColossus()
+    //creat class colossus
+    class colossus {
+        // creat function colossus
+        func creatColossus() {
+            var colossus = Colossus()
+            colossus.name = "jacques"
+            colossus.life = 180
+            colossus.attack = 5
+            
+            creatColossus()
+        }
     }
-    
-    // creat function Dwarf
-    func creatDwarf() {
-        var dwarf = Dwarf()
-        dwarf.name = "michel"
-        dwarf.life = 70
-        dwarf.attack = 25
-        
-        creatDwarf()
+    //creat class dwarf
+    class dwarf {
+        // creat function Dwarf
+        func creatDwarf() {
+            var dwarf = Dwarf()
+            dwarf.name = "michel"
+            dwarf.life = 70
+            dwarf.attack = 25
+            
+            creatDwarf()
+        }
     }
-    
 }
 
