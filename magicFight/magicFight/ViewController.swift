695044230b2e75705d21ffb95df51923de656618
MrNasch <nicolaspaschal@hotmail.com>
2018-04-09 11:30:51 +0200

    added all the caracters

--- magicFight/magicFight/ViewController.swift
+++ magicFight/magicFight/ViewController.swift
@@ -10,6 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // created var newGame imported when you creat a new game
     var newGame : String = "Welcome to MagicFight please select 3 caracters"
+    
+    // creat the fighter
+    var fighter = Fighter()
+    fighter.name = "pierre"
+    fighter.life = 100
+    fighter.attack = 10
+    
+    // creat the magus
+    var magus = Magus()
+    magus.name = "paul"
+    magus.life = 120
+    magus.heal = 8
+    
+    // creat the colossus
+    var colossus = Colossus()
+    colossus.name = "jacques"
+    colossus.life = 180
+    colossus.attack = 5
+    
+    // creat the Dwarf
+    var dwarf = Dwarf()
+    dwarf.name = "michel"
+    dwarf.life = 70
+    dwarf.attack = 25
 }
 
