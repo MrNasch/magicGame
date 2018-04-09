3082d319a876b28eef85a833d3191c24025fad74
MrNasch <nicolaspaschal@hotmail.com>
2018-04-09 10:58:48 +0200

    created magicFight folder and basic files

---
+++ magicFight/magicFight/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  magicFight
+//
+//  Created by Paschal on 9/04/18.
+//  Copyright © 2018 Nasch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
